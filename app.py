e3d358169bfbc31717978bb438c45d2e19ea4219
matif1766095 <163998768+matif1766095@users.noreply.github.com>
2024-11-03 03:59:27 +0500

    added main file app.py
    here all the backed code will handled

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+import csv
+from io import StringIO
+import pandas as pd
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a random secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game_coins.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Redirect to login page when unauthorized
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
