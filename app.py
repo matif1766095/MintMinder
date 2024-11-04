b0e54d0cb51d04e91cc92d428cee14ff92949e2b
matif1766095 <163998768+matif1766095@users.noreply.github.com>
2024-11-04 23:26:52 +0500

    Update app.py
    Added User model in app.py to create user table

--- app.py
+++ app.py
@@ -16,6 +16,16 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # Redirect to login page when unauthorized
 
+# User model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    transactions = db.relationship('TransactionHistory', backref='user', lazy=True)
+    coins = db.relationship('GameCoin', backref='owner', lazy=True)
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
