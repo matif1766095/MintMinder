b5f69452d6a96542318f1fb0b60527668feaee14
matif1766095 <163998768+matif1766095@users.noreply.github.com>
2024-11-14 21:41:28 +0000

    implemented the login functionality test by unit test

---
+++ loginUnitTest.py
@@ -0,0 +1,25 @@
+import unittest
+from app import app, db 
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app 
+        self.client = self.app.test_client()
+        self.app.config['TESTING'] = True  
+        with self.app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove() 
+            db.drop_all() 
+
+    def test_login(self):
+        response = self.client.post('/login', data={
+            'email': 'test@example.com',
+            'password': 'password'
+        })
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
